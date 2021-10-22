95e3a7593be4d9713f413fe091bc80ba1175984e
Matthew Holroyd <mholroyd@finchoice.co.za>
2021-10-22 10:48:37 +0200

    Add Config

---
+++ src/infra/config.ts
@@ -0,0 +1,54 @@
+import * as convict from 'convict'
+
+export const config = getConfig()
+config.validate({ allowed: 'strict' })
+
+function getConfig(): convict.Config<any> {
+  return convict({
+    env: {
+      doc: 'The deployment environment',
+      format: ['live', 'local'],
+      env: 'ENV',
+      default: 'local'
+    },
+    componentName: {
+      doc: 'Component name to use in metrics and logging',
+      format: String,
+      env: 'COMPONENT_NAME',
+      default: 'dazn-coding-challenge'
+    },
+    port: {
+      doc: 'Port for starting the app on.',
+      format: 'port',
+      env: 'PORT',
+      default: 8000
+    },
+    logLevel: {
+      doc: 'Log level to start logging at.',
+      format: ['debug', 'info', 'warn', 'error'],
+      env: 'LOG_LEVEL',
+      default: 'debug'
+    },
+    statsd: {
+      host: {
+        doc: 'StatsD server host',
+        format: String,
+        env: 'STATSD_HOST',
+        default: 'localhost'
+      },
+      port: {
+        doc: 'StatsD server port',
+        format: 'port',
+        env: 'STATSD_PORT',
+        default: 8125
+      }
+    },
+    dynamoDb: {
+      endpoint: {
+        default: '',
+        env: 'AWS_DYNAMODB_ENDPOINT',
+        format: String
+      }
+    }
+  })
+}
